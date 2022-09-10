00fdaab96b931b290434e493e688a4c21f3efbfc
Urge2247 <urge2247@gmail.com>
2022-09-10 13:48:42 -0700

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the string in C program
+ *
+ * Description: using the main function
+ * this program prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return 1
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return (1);
+};
+
