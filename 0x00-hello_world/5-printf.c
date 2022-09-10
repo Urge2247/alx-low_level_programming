2df8890cd24f61d04ce118d50e71149253fa19bb
Urge2247 <urge2247@gmail.com>
2022-09-09 17:53:47 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,13 @@
-#include<stdio.h>                                                                          
-
-/**                                                                                        
-
-*main - print the string in the printf                                                     
-
-*                                                                                          
-
-*Description using the main function                                                       
-
-*this program prints with proper grammar, but the outcome is a piece of art,               
-
-*Return: 0                                                                                 
-
-*/                                                                                         
-
-int main(void)                                                                             
-
-{                                                                                          
-
-        printf("with proper grammar, but the outcome is a piece of art,");
+#includ<stdio.h>
+/**
+*main - prints the string in printf function
+*
+*Description: Using the main function
+*this program prints with proper grammar, but the outcome is a piece of art,
+*Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
