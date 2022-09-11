1fe9bdbfecfb4f6dd41e3c809ccdb149f42411bd
Urge2247 <urge2247@gmail.com>
2022-09-10 18:29:19 -0700

    C program variables else if while with readme

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,17 +6,10 @@
 /* betty style doc for function main goes there */
 /**
  *
- *  * main - Entry point
- *
- *   * 
- *
- *    * Description: 'the program's description'
- *
- *     * 
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
