d4ee42016786771264a31f1a87b222ae2a2d5473
Urge2247 <urge2247@gmail.com>
2022-09-11 16:17:45 -0700

    2-last_digit.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
  * Description: 'using the main program's description'
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char ch = 'a';
-	
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
