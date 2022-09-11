fa5174355d59ce01cd9ffb05aaa8798734f4c5a6
Urge2247 <urge2247@gmail.com>
2022-09-10 19:19:47 -0700

    task 1 and 2 C Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'using the main program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	char ch = 'a';
+	while(ch <= 'z' )
+	{
+		putchar("%c ", ch);
+		ch++;
+	}
+	return (0);
+}
