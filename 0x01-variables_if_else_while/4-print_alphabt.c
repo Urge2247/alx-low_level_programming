9575fb53629fa416dd5bab0917334bb472581b40
Urge2247 <urge2247@gmail.com>
2022-09-11 17:34:48 -0700

    4-print alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: 'using the main program's description'
+* Return: Always 0 (Success)
+*/
+int main(void) 
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+
+		if(ch != 'e' && ch != 'q')
+		
+			putchar(ch);
+			ch++;
+
+	putchar('\n');
+
+	return (0);
+}
