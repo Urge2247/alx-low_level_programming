6600d0915a0a429e41764459149a0b57666c7b7f
Urge2247 <urge2247@gmail.com>
2022-09-12 15:27:22 -0700

    101-qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main - prints the string in C program
- *
- * Description: using the main function
- * this program prints and that piece of art is useful" - Dora Korpar, 2015-10-19
- * Return (1)
- */
+* main - prints the string in C program
+*
+* Description: using the main function
+* this program prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+* Return (1)
+*/
 int main(void)
 {
+
 	write(fno, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
