3a9eb676a4ea82227d35a174ef6f1294e2381b3a
Urge2247 <urge2247@gmail.com>
2022-09-12 15:45:25 -0700

    101-qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
 * main - prints the string in C program
 *
 * Description: using the main function
-* this program prints and that piece of art is useful" - Dora Korpar, 2015-10-19
-* Return: (1)
+* this program prints and that piece of art is useful" - Dora Korpar, 2015-10-1
+* Return: (1) 
 */
 int main(void)
 {
