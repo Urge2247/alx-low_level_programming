1afdd8556de702c916bb24f6eca4e2d606d765fd
Urge2247 <urge2247@gmail.com>
2022-09-12 16:21:49 -0700

    8-print_hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints the hexadecimal in lowercase the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int n;
+	char ch;
+
+	for (n = 0; n <= 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
