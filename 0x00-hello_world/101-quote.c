d35acd2b83a2eaf8600cb177768f09ac900ede2b
Urge2247 <urge2247@gmail.com>
2022-09-12 15:33:51 -0700

    101-qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 
-	write(fno, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(s_fno, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
