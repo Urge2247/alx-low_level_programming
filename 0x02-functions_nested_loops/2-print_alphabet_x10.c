01846a05bde8915306ae109b661e13bf2cd92576
Urge2247 <urge2247@gmail.com>
2022-09-14 16:26:27 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,20 @@
-#include <unistd.h>
-
-
+#include "main.h"
 
 /**
+ * print_alphabet_x10 - prints x10 alphabets in lowercase
  *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
-
-int _putchar(char c)
-
+ * Return: 0
+ */
+void print_alphabet_x10(void)
 {
+	char ch;
+	int n;
 
-		return (write(1, &c, 1));
+	for (n = 1; n <= 10; n++)
 
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+
+
+	_putchar('\n');
 }
