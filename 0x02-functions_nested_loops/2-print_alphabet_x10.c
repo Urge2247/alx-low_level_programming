0dd94089341376bc900a14b7017c007a35d137c3
Urge2247 <urge2247@gmail.com>
2022-09-14 16:35:14 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,13 @@ void print_alphabet_x10(void)
 	int n;
 
 	for (n = 1; n <= 10; n++)
-
+	{
 		for (ch = 'a'; ch <= 'z'; ch++)
+		{
 			_putchar(ch);
+		}
 
 
-			_putchar('\n');
+		_putchar('\n');
+	}
 }
