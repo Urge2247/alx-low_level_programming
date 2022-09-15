53c68e0543b84431aebc0bb3e0ec0026635d490a
Urge2247 <urge2247@gmail.com>
2022-09-14 17:03:23 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,16 @@
+#include "main.h"
 
+/**
+ * test_isalpha - Test the _isalpha function
+ *
+ * @c: Number to pass to _isalpha function
+ *
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <=  'Z'))
+		return (1);
+	else
+		return (0);
+}
