e0db51cd7da088d39c34052e71cdae8a16a8c034
Urge2247 <urge2247@gmail.com>
2022-09-14 17:23:51 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,17 +7,22 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
 		_putchar('\n');
+	}
 
 	else if (n == 0)
+	{
 		_putchar('0');
 		return (0);
 		_putchar('\n');
+	}
 	else
+	{	
 		_putchar('-');
-		return (-);
+		return (-1);
 		_putchar('\n');
-
+	}
 }
