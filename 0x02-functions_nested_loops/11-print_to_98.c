5d96888d7f96160bb109714050c23ff184d6d628
Urge2247 <urge2247@gmail.com>
2022-09-15 15:29:35 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_to_98 - prints numbers from 0 to 98
+ * @n: an int to be added
+ *
+ * Return: 0_to_98
+ */
+void print_to_98(int n)
+{
+	if (n >= )
+
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	else
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+}
