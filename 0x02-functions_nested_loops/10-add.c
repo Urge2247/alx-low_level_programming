8803dbea67fcfa86e8ad661b2ea18e5bdfb8fca4
Urge2247 <urge2247@gmail.com>
2022-09-15 15:19:14 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - prints addition of 2 numbers
+ * @n1: int to be added to fun
+ * @n2: another int tobe added
+ *
+ * Return: added
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
