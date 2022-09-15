0694658635dbf7049748398577ac2234075207c3
Urge2247 <urge2247@gmail.com>
2022-09-15 16:36:08 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - prints product of two numbers
+ * @a: int to be added to fun
+ * @b: another int to be added
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
