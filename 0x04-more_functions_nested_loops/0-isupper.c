4020e8ce9a0741e5f0e304041c532b97777debf5
Urge2247 <urge2247@gmail.com>
2022-09-15 16:18:03 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,12 @@
 /**
  * _isupper - prints uppercase letters
  * @c: int to be added to fun
+ *
+ * Return: 0
  */
 int _isupper(int c)
 {
-	if (c = 'A' && c = 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
