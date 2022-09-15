8d1bd7ced246507bbe644b19eb1f878439968946
Urge2247 <urge2247@gmail.com>
2022-09-14 17:19:43 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+		_putchar('\n');
+
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+		_putchar('\n');
+	else
+		_putchar('-');
+		return (-);
+		_putchar('\n');
+
+}
