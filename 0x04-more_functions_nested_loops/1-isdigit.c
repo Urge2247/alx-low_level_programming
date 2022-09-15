34fa7896ac482d3ebd8e0b17c52193fb04b3a3d9
Urge2247 <urge2247@gmail.com>
2022-09-15 16:24:53 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a number 0 to 9
+ * @c: int to be added to fun
+ *
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 0)
+		return (1);
+	else
+		return (0);
+}
