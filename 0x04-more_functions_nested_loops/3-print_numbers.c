68abfce3b3c97e61210e268df5cfab90582c0c58
Urge2247 <urge2247@gmail.com>
2022-09-16 15:39:19 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,12 @@
 #include "main.h"
 /**
  * print_numbers - prints numbers from 0 to 9 followed
+ * @i: int to be added to fun
  * Return: nothing
  */
 void print_numbers(void)
 {
-	int i =28;
+	int i = 28;
 
 	while (i <= 38)
 	{
