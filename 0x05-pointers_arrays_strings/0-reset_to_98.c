c1329c52bdcb2d0b850a99a6bf701130bf67e246
Urge2247 <urge2247@gmail.com>
2022-09-19 16:54:15 -0700

    My commits on pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - prints reset to 98
+ * @n: int to be added to fun
+ * Return: returns void.
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int *p;
+
+	*p = 98;
+}
