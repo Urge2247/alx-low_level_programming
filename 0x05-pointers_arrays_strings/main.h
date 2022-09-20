f5b71da15e8ebe687cef83e219ca2f5df4b0336e
Urge2247 <urge2247@gmail.com>
2022-09-19 17:08:12 -0700

    My commits on pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,4 +12,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#ENDIF
+#endif
