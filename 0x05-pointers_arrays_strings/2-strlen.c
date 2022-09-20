a8880fdd802ed874d93f7642e8c54e9f21f415a3
Urge2247 <urge2247@gmail.com>
2022-09-20 16:15:06 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,17 +3,11 @@
 #include "main.h"
 /**
  *_strlen - prints length of string
- *@str: a char param
  *@s: a char to be added to fun
  *Return: 0
  */
 int _strlen(char *s)
 {
-	char *str;
-	int len;
-
-	_strlen(s);
-	len = _strlen(*str);
-
+	strlen(s);
 	return (0);
 }
