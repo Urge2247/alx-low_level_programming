8c01adba8c898524b6cb008506604603cc0926ad
Urge2247 <urge2247@gmail.com>
2022-09-20 15:18:50 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include "main.h"
+/**
+ *swap_int - prints swaps of 2 ints
+ *@a: an int to be added to fun
+ *@b: another int to be added
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
