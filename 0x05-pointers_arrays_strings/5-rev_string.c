97e4431d8f861e9847ae910a34743ec21584ae26
Urge2247 <urge2247@gmail.com>
2022-09-21 17:41:15 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * rev_string - prints reverse of str
+ * @s: string to be added for printing
+ */
+void rev_string(char *s)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < strlen(s) / 2; i++)
+		c = s[i];
+		s[i] = s[strlen(s) - i - 1];
+		s[strlen(s) - i - 1] = c;
+}
