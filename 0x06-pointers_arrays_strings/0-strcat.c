d68398c051eb1601f9eb0eb373bbd537a50ceef0
Urge2247 <urge2247@gmail.com>
2022-09-22 16:27:32 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - prints concatenation of string
+ * @dest: a char to be added to fun
+ * @src: another char to be added
+ * Return: returns a pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);	
+}
