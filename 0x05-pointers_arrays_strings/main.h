b61b615cfc55469356485ccea7bb8e7758e0e977
Urge2247 <urge2247@gmail.com>
2022-09-22 17:24:14 -0700

    added newer changes in this dir

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+#include <stdio.h>
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,4 +14,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif
+#endif /* MAIN_H */
