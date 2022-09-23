e0b7a63846626a7b7b310fa236c77c2124e54af8
Urge2247 <urge2247@gmail.com>
2022-09-23 15:57:02 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - prints copies length of one string to another string
+ *
+ * @dest: a destination string to be added to fun
+ * @src: another source string to be added
+ * @n: an integer to be added
+ *
+ * Return: pointer to destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	int length1 = 0;
+
+	while (*(src + length1) != '\0')
+		length1++;
+
+	for (i = 0; i < length1 && i < n; i++)
+	{
+		*(dest + (i)) = *(src + i);
+	}
+	if (length1 < n)
+	{
+		for (j = 0; length1 < n; j++, length1++)
+		{
+			*(dest + (i + j)) = '\0';
+		}
+	}
+	return (dest);
+}
+
