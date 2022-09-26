9c912529b726cbd4fcfc442a35e584adbd9475b1
Urge2247 <urge2247@gmail.com>
2022-09-25 17:23:30 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,1 +1,9 @@
-#
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H_ */
